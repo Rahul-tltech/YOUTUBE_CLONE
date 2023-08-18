ef89c6f4bed16f3af115fb02d705251baa189c68
Rahul-tltech <136077769+Rahul-tltech@users.noreply.github.com>
2023-08-18 21:44:36 +0530

    fetching youtube api for videos

--- src/components/Videocontainer.js
+++ src/components/Videocontainer.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { YOUTUBE_VIDEO_API } from '../utils/constants'
 
 const Videocontainer = () => {
+  useEffect(()=>{
+    getVideos()
+  },[])
+  const getVideos = async()=>{
+    const data = await fetch( YOUTUBE_VIDEO_API)
+    const  response = await data.json()
+    console.log(response)
+  }
   return (
     <div className='mt-2'>
       Videocontainer
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 const API_KEY = 'AIzaSyBKhmoGWpk4KYCFFvJpFZUQvHXfWvvwpMQ'
 
-export const YOUTUBE_VIDEO_API = 
+export const YOUTUBE_VIDEO_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${API_KEY}` 
